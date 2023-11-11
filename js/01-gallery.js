93251a01e1c007fa4cf22553f49edf77dd11fe4a
Tanya Yenakieva <yastigma@gmail.com>
2023-11-11 21:42:34 +0200

    fix task 1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,5 +1,4 @@
 import { galleryItems } from './gallery-items.js';
-// Change code below this line
 
 const list = document.querySelector(".gallery");
 
@@ -9,95 +8,66 @@ list.addEventListener("click", handleClick);
 let instance = null;
 
 function handleClick(event) {
-    event.preventDefault();
+  event.preventDefault();
   if (event.target === event.currentTarget) {
     return;
   }
-    const currentGallery = event.target.closest(".gallery__item");
     
- const currentGalleryOriginal = currentGallery.querySelector("img").dataset.source;
-const gallery = galleryItems.find(({ original }) => original === currentGalleryOriginal);
-console.log(gallery);
+  const currentGallery = event.target.closest(".gallery__item");
+  const currentGalleryOriginal = currentGallery.querySelector("img").dataset.source;
+  const gallery = galleryItems.find(({ original }) => original === currentGalleryOriginal);
 
-    
-       
+  console.log(gallery);
+
   instance = basicLightbox.create(`
-  <div class="modal">
-    <img 
-      src="${gallery.original}"
-   
-      alt="${gallery.description}"
-      </div>
-  `);
+        <div class="modal">
+            <img 
+                src="${gallery.original}"
+                alt="${gallery.description}"
+                width="800">
+        </div>
+    `);
+  
+  instance.show();
 
-    instance.show();
-    
-    document.addEventListener("keydown", handleKeyDown);
+  instance.element().addEventListener("click", handleCloseClick);
+ 
+  instance.element().removeEventListener("close", handleModalClose);
+}
+function handleCloseClick() {
+    instance.close();
 }
 
+function handleModalClose() {
+    instance.element().removeEventListener("click", handleCloseClick);
+    instance.off("close", handleModalClose);
+}
 
 function handleKeyDown(event) {
-  if (event.key === "Escape") {
-    instance.close(); 
-  }
+    if (event.key === "Escape") {
+        instance.close(); 
+    }
 }
-
-instance.on("visible", () => {
-  document.addEventListener("keydown", handleKeyDown);
-});
-
-instance.on("hidden", () => {
-  document.removeEventListener("keydown", handleKeyDown);
-});
-
-
-
 
 function createMarkup(arr) {
-  return arr
-    .map(
-      ({ preview, original, description }) => `
-  <li class="gallery__item">
-    <a class="gallery__link" href="${original}">
-    <img
-      class="gallery__image"
-      src="${preview}"
-      data-source="${original}"
-      alt="${description}"
-    />
-  </a>
-</li>
-`
-    )
-    .join("");
+    return arr
+        .map(
+            ({ preview, original, description }) => `
+            <li class="gallery__item">
+                <a class="gallery__link" href="${original}">
+                    <img
+                        class="gallery__image"
+                        src="${preview}"
+                        data-source="${original}"
+                        alt="${description}"
+                    />
+                </a>
+            </li>
+            `
+        )
+        .join("");
 }
 
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
-
 console.log(galleryItems);
+
+
